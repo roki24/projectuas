1c8d29223af5a0fb64d2233b65bece23e34cf890
Anagis6 <marknupiadrian8@gmail.com>
2025-06-16 21:42:56 +0700

    benerin routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,22 +18,10 @@ $routes->post('/login', 'Auth::login');                   // Proses login
 $routes->get('/logout', 'Auth::logout');                  // Proses logout
 $routes->get('/admin', 'Dashboard::admin');
 $routes->get('/customer', 'Dashboard::customer');
-// Halaman dashboard setelah login (contoh: untuk customer)
 $routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
-// Halaman utama daftar motor
 $routes->get('/daftar_motor', 'AdminController::daftar_motor');
-
-// Tambah motor (form)
 $routes->get('/motor/tambah', 'AdminController::tambah_motor');
-
-// Simpan motor baru (POST)
 $routes->post('/motor/simpan', 'AdminController::simpan_motor');
-
-// Edit motor (form)
 $routes->get('/motor/edit/(:num)', 'AdminController::edit_motor/$1');
-
-// Update motor (POST)
 $routes->post('/motor/update/(:num)', 'AdminController::update_motor/$1');
-
-// Hapus motor
 $routes->get('/motor/hapus/(:num)', 'AdminController::hapus_motor/$1');
